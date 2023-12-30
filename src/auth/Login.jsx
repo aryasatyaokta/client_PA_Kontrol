3623ad5dcf9ac0e8282f065f3c9d8e1048b043e7
aryaganteng <hadi.argaya@gmail.com>
2023-12-30 11:08:59 +0700

    commit new 3

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -87,7 +87,7 @@ export default function Login() {
             </div>
       
             <div>
-              <button type='submit' className="flex w-full justify-center rounded-md bg-[#9A3B3B] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#6F2525] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Masuk</button>
+              <button type="button" onClick={handleSubmit} className="flex w-full justify-center rounded-md bg-[#9A3B3B] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#6F2525] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Masuk</button>
             </div>
           </form>
       
